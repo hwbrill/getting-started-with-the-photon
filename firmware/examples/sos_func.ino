136b94886ae82eb5191c29056761e13864f0679f
hwbrill <hwbrill@brill6pak.com>
2016-06-06 16:11:10 -0500

    added program
    sos_func

---
+++ firmware/examples/sos_func.ino
@@ -0,0 +1,30 @@
+void setup () {
+	// Set Photon's built in RGB LED to be controlled by User's code
+	RGB.control(true);
+}
+
+void loop () {
+	// flash 'S' ...
+	flash(200,200);
+	flash(200,200);
+	flash(200,600);
+	// flash 'O' ---
+	flash(600,200);
+	flash(600,200);
+	flash(600,600);
+	// flash 'S' ...
+	flash(200,200);
+	flash(200,200);
+	flash(200,600);
+
+	delay(2000); // delay before repeat
+}
+
+void flash(int duration, int gap) {
+
+RGB.color(255,255,255);
+delay(duration);
+RGB.color(0,0,0);
+delay(gap);
+
+}
