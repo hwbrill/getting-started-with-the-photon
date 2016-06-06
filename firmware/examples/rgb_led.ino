e8700c0f4fc25a1180cd0ec992e2cbca7a8caa89
hwbrill <hwbrill@brill6pak.com>
2016-06-06 12:58:16 -0500

    Comment
    Added commenting to code

--- firmware/examples/rgb_led.ino
+++ firmware/examples/rgb_led.ino
@@ -1,12 +1,17 @@
 void setup () {
+	// Set Photon's built in RGB LED to be controlled by User's code
 	RGB.control(true);
 }
 
 void loop () {
+	// Red
 	RGB.color(255,0,0);
+	// Delay 2 seconds (typical)
 	delay(2000);
+	// Green
 	RGB.color(0,255,0);
 	delay(2000);
+	// Blue
 	RGB.color(0,0,255);
 	delay(2000);
 }
